58b483d234578e64d80b5fdd6aee28d7aea91d9c
Alberto Rodriguez Garcia <agarcia@schubergphilis.com>
2020-04-27 22:31:30 +0200

    WIP

--- dns.tf
+++ dns.tf
@@ -15,10 +15,10 @@ output "nginx-ingress-lb" {
   value = [data.kubernetes_service.nginx-ingress-controller.load_balancer_ingress]
 }
 
-#resource "aws_route53_record" "api" {
-#  zone_id = lookup(var.aws_route53, "zone_id")
-#  name    = "*.${lookup(var.aws_route53, "domain")}"
-#  type    = lookup(var.aws_route53, "type")
-#  ttl     = lookup(var.aws_route53, "ttl")
-#  records = [data.kubernetes_service.nginx-ingress-controller.load_balancer_ingress.0.ip]
-#}
+resource "aws_route53_record" "api" {
+  zone_id = lookup(var.aws_route53, "zone_id")
+  name    = "*.${lookup(var.aws_route53, "domain")}"
+  type    = lookup(var.aws_route53, "type")
+  ttl     = lookup(var.aws_route53, "ttl")
+  records = [data.kubernetes_service.nginx-ingress-controller.load_balancer_ingress.0.hostname]
+}
